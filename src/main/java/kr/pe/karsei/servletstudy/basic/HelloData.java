1f948c18c22639b3a9434335a61dc598d5339bd8
Karsei <karseidev@gmail.com>
2022-10-03 14:26:59 +0900

    feat: added request servlet with json message

---
+++ src/main/java/kr/pe/karsei/servletstudy/basic/HelloData.java
@@ -0,0 +1,11 @@
+package kr.pe.karsei.servletstudy.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloData {
+    private String username;
+    private int age;
+}
